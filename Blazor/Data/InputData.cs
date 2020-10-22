5a19dd406c27d91986fda830efd8d7bae95ca563
tdadah <tara.dadah@gmail.com>
2020-10-22 19:51:26 -0400

    Initial commit of Blazor implementation.

---
+++ Blazor/Data/InputData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+using FizzBuzz.Models;
+
+namespace BlazorFizzBuzz.Data
+{
+	public class InputData
+	{
+		public InputData()
+		{
+			MultipleValues = new List<MultipleValue>
+			{
+				new MultipleValue( 3, "Fizz" ),
+				new MultipleValue( 5, "Buzz" ),
+				new MultipleValue( 7, "Wizz" ),
+				new MultipleValue( 11, "Bang" )
+			};
+		}
+
+		public List<MultipleValue> MultipleValues
+		{
+			get;
+			set;
+		}
+	}
+}
